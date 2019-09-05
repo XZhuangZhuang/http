fa3c3911edc90ce8a0df23c5600a2d08376e79cf
XZhuangZhuang <38785646+XZhuangZhuang@users.noreply.github.com>
2019-09-05 22:46:50 +0800

    Update http_conn.cpp

--- http_conn.cpp
+++ http_conn.cpp
@@ -13,6 +13,7 @@ const char* error_500_form = "There was an unusual problem serving the requested
 /*网站根目录*/
 const char* doc_root = "/var/www/html";
 
+//设置文件描述符为非阻塞
 int setnonblocking( int fd )
 {
     int old_option = fcntl( fd, F_GETFL );
@@ -20,7 +21,7 @@ int setnonblocking( int fd )
     fcntl( fd, F_SETFL, new_option );
     return old_option;
 }
-
+//在epoll事件表中添加新的文件描述符，同时采用ET模式，并且设置EPOLLONESHOT 属性：任一时刻，socket连接只能被一个线程所处理
 void addfd( int epollfd, int fd, bool one_shot )
 {
     epoll_event event;
@@ -33,13 +34,14 @@ void addfd( int epollfd, int fd, bool one_shot )
     epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &event );
     setnonblocking( fd );
 }
-
+//在epoll事件表中删除新的文件描述符，并且关闭文件描述符
 void removefd( int epollfd, int fd )
 {
     epoll_ctl( epollfd, EPOLL_CTL_DEL, fd, 0 );
     close( fd );
 }
 
+//EPOLLONESHOT需要重置事件后事件才能进行下次侦听
 void modfd( int epollfd, int fd, int ev )
 {
     epoll_event event;
@@ -48,9 +50,10 @@ void modfd( int epollfd, int fd, int ev )
     epoll_ctl( epollfd, EPOLL_CTL_MOD, fd, &event );
 }
 
-int http_conn::m_user_count = 0;
-int http_conn::m_epollfd = -1;
+int http_conn::m_user_count = 0;//连接数
+int http_conn::m_epollfd = -1;//事件表，注意是static故所有http_con类对象共享一个事件表
 
+//关闭连接，从事件表中移除描述符
 void http_conn::close_conn( bool real_close )
 {
     if( real_close && ( m_sockfd != -1 ) )
@@ -69,20 +72,20 @@ void http_conn::init( int sockfd, const sockaddr_in& addr )
 
     int error = 0;
     socklen_t len = sizeof( error );
-    getsockopt( m_sockfd, SOL_SOCKET, SO_ERROR, &error, &len );
+    getsockopt( m_sockfd, SOL_SOCKET, SO_ERROR, &error, &len );//获取与某个套接字关联的选项
 	/*以下两行是为了避免TIME_WAIT状态，仅用于调试，实际使用时应该去掉*/
     int reuse = 1;
-    setsockopt( m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof( reuse ) );
-	
-    addfd( m_epollfd, sockfd, true );
-    m_user_count++;
+    setsockopt( m_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof( reuse ) );//设置与某个套接字关联的选项
+     //一个端口释放后会等待两分钟之后才能再被使用，SO_REUSEADDR是让端口释放后立即就可以被再次使用。
+    addfd( m_epollfd, sockfd, true );//在事件表中添加连接套接字
+    m_user_count++;//用户数+1
 
     init();
 }
 
-void http_conn::init()
+void http_conn::init()//初始化其他shttp_conn类的成员变量
 {
-    m_check_state = CHECK_STATE_REQUESTLINE;
+    m_check_state = CHECK_STATE_REQUESTLINE;//主状态机当前所处的状态初始为正在解析请求行
     m_linger = false;
 
     m_method = GET;
@@ -99,7 +102,7 @@ void http_conn::init()
     memset( m_real_file, '\0', FILENAME_LEN );
 }
 
-/*从状态机*/
+/*从状态机*/  //用于解析出一行内容
 http_conn::LINE_STATUS http_conn::parse_line()
 {
     char temp;
@@ -145,6 +148,7 @@ http_conn::LINE_STATUS http_conn::parse_line()
     return LINE_OPEN;
 }
 /*循环读取客户数据，直到无数据可读或者对方关闭连接*/
+//读取客户端发送过来的数据，将其存放到m_read_buf缓冲区中
 bool http_conn::read()
 {
     if( m_read_idx >= READ_BUFFER_SIZE )
@@ -155,10 +159,13 @@ bool http_conn::read()
     int bytes_read = 0;
     while( true )
     {
-        bytes_read = recv( m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0 );
+        bytes_read = recv( m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0 );//循环读取套接字描述符发送来的数据，缓存到m_read_buf中
         if ( bytes_read == -1 )
         {
-            if( errno == EAGAIN || errno == EWOULDBLOCK )
+            //以非阻塞O_NONBLOCK的标志打开文件/socket/FIFO，如果你连续做read操作而没有数据可读。
+            /*	此时程序不会阻塞起来等待数据准备就绪返回，read函数会返回一个错误EAGAIN，提示你的应用程序现在没有数据可读请稍后再试。*/
+            //对非阻塞socket而言，EAGAIN不是一种错误。在VxWorks和Windows上，EAGAIN的名字叫做EWOULDBLOCK。
+	    if( errno == EAGAIN || errno == EWOULDBLOCK )
             {
                 break;
             }
@@ -176,7 +183,8 @@ bool http_conn::read()
 /*解析HTTP请求行，获得请求方法、目标URL，以及HTTP版本号*/
 http_conn::HTTP_CODE http_conn::parse_request_line( char* text )
 {
-    m_url = strpbrk( text, " \t" );
+    //strpbrk(s1，s2)从s1的第一个字符向后检索，直到'\0'，如果当前字符存在于s2中，那么返回当前字符的地址，并停止检索。
+	m_url = strpbrk( text, " \t" );//\t是水平制表符，相当于TAB按键
 	/*如果请求行中没有空白字符或“\t”，则HTTP请求必有问题*/
     if ( ! m_url )
     {
@@ -185,6 +193,8 @@ http_conn::HTTP_CODE http_conn::parse_request_line( char* text )
     *m_url++ = '\0';
 
     char* method = text;
+     //strcasecmp(s1,s2)用来比较参数s1 和s2 字符串，比较时会自动忽略大小写的差异。
+    //返回值：若参数s1 和s2 字符串相同则返回0。s1 长度大于s2 长度则返回大于0 的值，s1 长度若小于s2 长度则返回小于0 的值。
     if ( strcasecmp( method, "GET" ) == 0 )
     {
         m_method = GET;
@@ -350,23 +360,23 @@ http_conn::HTTP_CODE http_conn::do_request()
     strcpy( m_real_file, doc_root );
     int len = strlen( doc_root );
     strncpy( m_real_file + len, m_url, FILENAME_LEN - len - 1 );
-    if ( stat( m_real_file, &m_file_stat ) < 0 )
+    if ( stat( m_real_file, &m_file_stat ) < 0 )//stat函数通过文件名filename获取文件信息，并保存在buf所指的结构体stat中
     {
         return NO_RESOURCE;
     }
 
-    if ( ! ( m_file_stat.st_mode & S_IROTH ) )
+    if ( ! ( m_file_stat.st_mode & S_IROTH ) )//若文件没有其他用户读权限
     {
         return FORBIDDEN_REQUEST;
     }
 
-    if ( S_ISDIR( m_file_stat.st_mode ) )
+    if ( S_ISDIR( m_file_stat.st_mode ) )//若文件是目录
     {
         return BAD_REQUEST;
     }
 
-    int fd = open( m_real_file, O_RDONLY );
-    m_file_address = ( char* )mmap( 0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0 );
+    int fd = open( m_real_file, O_RDONLY );//以只读的方式open
+    m_file_address = ( char* )mmap( 0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0 );//将文件mmap映射，PROT_READ 映射区域可被读取，对映射区域的写入操作会产生一个映射文件的复制，即私人的“写入时复制”（copy on write）对此区域作的任何修改都不会写回原来的文件内容。
     close( fd );
     return FILE_REQUEST;
 }
